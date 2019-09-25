a69e1f9284b1020caccec3b1f9871dad3cb1ed09
ngangavic <ngangavictor10@gmail.com>
2019-09-25 10:14:28 +0300

    mpesa

--- app/src/main/java/com/example/mpesaapi/mpesa/BusinessCustomer.java
+++
@@ -1,4 +0,0 @@
-package com.example.mpesaapi.mpesa;
-
-public class BusinessCustomer {
-}
---
+++ app/src/main/java/com/example/mpesaapi/mpesa/Mpesa.java
@@ -0,0 +1,188 @@
+package com.example.mpesaapi.mpesa;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import static com.example.mpesaapi.network.Network.sendRequest;
+
+public class Mpesa {
+
+
+    //b2c request
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void businessCustomer(String initiatorName, String securityCredential, String commandID, String  amount, String partyA, String partyB, String remarks, String queueTimeOutURL, String resultURL, String occassion) throws IOException, JSONException {
+        JSONArray jsonArray=new JSONArray();
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("InitiatorName", initiatorName);
+        jsonObject.put("SecurityCredential", securityCredential);
+        jsonObject.put("CommandID", commandID);
+        jsonObject.put("Amount", amount);
+        jsonObject.put("PartyA", partyA);
+        jsonObject.put("PartyB", partyB);
+        jsonObject.put("Remarks", remarks);
+        jsonObject.put("QueueTimeOutURL", queueTimeOutURL);
+        jsonObject.put("ResultURL", resultURL);
+        jsonObject.put("Occassion", occassion);
+
+        jsonArray.put(jsonObject);
+
+        String requestJson=jsonArray.toString().replaceAll("[\\[\\]]","");
+        sendRequest(requestJson);
+    }
+
+    //b2b request
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void businessBusiness(String initiatorName, String accountReference, String securityCredential, String commandID, String senderIdentifierType, String receiverIdentifierType, float  amount, String partyA, String partyB, String remarks, String queueTimeOutURL, String resultURL, String occassion) throws IOException, JSONException {
+        JSONArray jsonArray=new JSONArray();
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("Initiator", initiatorName);
+        jsonObject.put("SecurityCredential", securityCredential);
+        jsonObject.put("CommandID", commandID);
+        jsonObject.put("SenderIdentifierType", senderIdentifierType);
+        jsonObject.put("RecieverIdentifierType",receiverIdentifierType);
+        jsonObject.put("Amount", amount);
+        jsonObject.put("PartyA", partyA);
+        jsonObject.put("PartyB", partyB);
+        jsonObject.put("Remarks", remarks);
+        jsonObject.put("AccountReference", accountReference);
+        jsonObject.put("QueueTimeOutURL", queueTimeOutURL);
+        jsonObject.put("ResultURL", resultURL);
+
+        jsonArray.put(jsonObject);
+
+        String requestJson=jsonArray.toString().replaceAll("[\\[\\]]","");
+        sendRequest(requestJson);
+    }
+
+    //c2b request
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void customerBusiness(String shortCode, String commandID, String amount, String MSISDN, String billRefNumber) throws IOException, JSONException {
+        JSONArray jsonArray=new JSONArray();
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("ShortCode", shortCode);
+        jsonObject.put("CommandID", commandID);
+        jsonObject.put("Amount", amount);
+        jsonObject.put("Msisdn", MSISDN);
+        jsonObject.put("BillRefNumber", billRefNumber);
+
+        jsonArray.put(jsonObject);
+
+        String requestJson=jsonArray.toString().replaceAll("[\\[\\]]","");
+
+        sendRequest(requestJson);
+    }
+
+    //stk push(Lipa Na Mpesa Online)
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void lipaNaMpesaOnline(String businessShortCode, String password, String timestamp, String transactionType, String amount, String phoneNumber, String partyA, String partyB, String callBackURL, String queueTimeOutURL, String accountReference, String transactionDesc) throws IOException, JSONException {
+        JSONArray jsonArray=new JSONArray();
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("BusinessShortCode", businessShortCode);
+        jsonObject.put("Password", password);
+        jsonObject.put("Timestamp", timestamp);
+        jsonObject.put("TransactionType", transactionType);
+        jsonObject.put("Amount",amount);
+        jsonObject.put("PhoneNumber", phoneNumber);
+        jsonObject.put("PartyA", partyA);
+        jsonObject.put("PartyB", partyB);
+        jsonObject.put("CallBackURL", callBackURL);
+        jsonObject.put("AccountReference", accountReference);
+        jsonObject.put("QueueTimeOutURL", queueTimeOutURL);
+        jsonObject.put("TransactionDesc", transactionDesc);
+
+        jsonArray.put(jsonObject);
+
+        String requestJson=jsonArray.toString().replaceAll("[\\[\\]]","");
+        sendRequest(requestJson);
+    }
+
+
+    //stk push query(Lipa Na Mpesa Online Query)
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void lipaNaMpesaOnlineQuery(String businessShortCode, String password, String timestamp, String checkoutRequestID) throws IOException, JSONException {
+        JSONArray jsonArray=new JSONArray();
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("BusinessShortCode", businessShortCode);
+        jsonObject.put("Password", password);
+        jsonObject.put("Timestamp", timestamp);
+        jsonObject.put("CheckoutRequestID", checkoutRequestID);
+
+        jsonArray.put(jsonObject);
+
+        String requestJson=jsonArray.toString().replaceAll("[\\[\\]]","");
+        sendRequest(requestJson);
+
+    }
+
+    //account balance
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void accountBalance(String initiator, String commandID, String securityCredential, String partyA, String identifierType, String remarks, String queueTimeOutURL, String resultURL) throws IOException, JSONException {
+        JSONArray jsonArray=new JSONArray();
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("Initiator", initiator);
+        jsonObject.put("SecurityCredential", securityCredential);
+        jsonObject.put("CommandID", commandID);
+        jsonObject.put("PartyA", partyA);
+        jsonObject.put("IdentifierType",identifierType);
+        jsonObject.put("Remarks", remarks);
+        jsonObject.put("QueueTimeOutURL", queueTimeOutURL);
+        jsonObject.put("ResultURL", resultURL);
+
+        jsonArray.put(jsonObject);
+
+        String requestJson=jsonArray.toString().replaceAll("[\\[\\]]","");
+        sendRequest(requestJson);
+    }
+
+    //registerUrl
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public  void registerURL(String shortCode, String responseType, String confirmationURL, String validationURL) throws IOException, JSONException {
+        JSONArray jsonArray=new JSONArray();
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("ShortCode", shortCode);
+        jsonObject.put("ResponseType", responseType);
+        jsonObject.put("ConfirmationURL", confirmationURL);
+        jsonObject.put("ValidationURL", validationURL);
+
+        jsonArray.put(jsonObject);
+
+        String requestJson=jsonArray.toString().replaceAll("[\\[\\]]","");
+        sendRequest(requestJson);
+    }
+
+
+    //reversal
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void reversal(String initiator, String securityCredential, String commandID, String transactionID, String amount, String receiverParty, String recieverIdentifierType, String resultURL, String queueTimeOutURL, String remarks, String ocassion) throws IOException, JSONException {
+        JSONArray jsonArray=new JSONArray();
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("Initiator", initiator);
+        jsonObject.put("SecurityCredential", securityCredential);
+        jsonObject.put("CommandID", commandID);
+        jsonObject.put("TransactionID", transactionID);
+        jsonObject.put("Amount",amount);
+        jsonObject.put("ReceiverParty", receiverParty);
+        jsonObject.put("RecieverIdentifierType", recieverIdentifierType);
+        jsonObject.put("QueueTimeOutURL", queueTimeOutURL);
+        jsonObject.put("ResultURL", resultURL);
+        jsonObject.put("Remarks", remarks);
+        jsonObject.put("Occasion", ocassion);
+
+        jsonArray.put(jsonObject);
+
+        String requestJson=jsonArray.toString().replaceAll("[\\[\\]]","");
+        sendRequest(requestJson);
+    }
+
+
+
+
+
+}
